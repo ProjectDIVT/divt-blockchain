63f648699e1b40bdb453d36f448fba82b1e1ae98
dikelito <dikelito@tutamail.com>
2018-02-20 21:35:27 +0200

    Created daemon and client

---
+++ src/RPC/DivtCli.java
@@ -0,0 +1,32 @@
+package RPC;
+
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class DivtCli {
+
+	public static void main(String[] args) {
+
+		try {
+			Registry registry = LocateRegistry.getRegistry(1099);
+			RMIInterface stub = (RMIInterface) registry.lookup("divt");
+
+			if (args.length < 1) {
+				//Print the help call
+				return;
+			}
+			switch (args[0]) {
+			//Add your call here :D
+			case "help":
+				//Print the help call
+				break;
+			default:
+				System.out.println("No such command");
+				break;
+			}
+		} catch (Exception e) {
+			System.err.println("Client exception: " + e.toString());
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/RPC/DivtDaemon.java
@@ -0,0 +1,37 @@
+package RPC;
+
+import java.rmi.AccessException;
+import java.rmi.AlreadyBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
+public class DivtDaemon implements RMIInterface {
+	
+	public static void main(String args[]) {
+		
+			Registry registry;
+			try {
+				registry = LocateRegistry.createRegistry(1099);
+				DivtDaemon object = new DivtDaemon();
+				RMIInterface stub;
+				stub = (RMIInterface) UnicastRemoteObject.exportObject(object, 0);
+				registry.bind("divt", stub);
+				System.out.println("Running");
+			} catch (AccessException e) {
+				e.printStackTrace();
+			} catch (RemoteException e) {
+				e.printStackTrace();
+			} catch (AlreadyBoundException e) {
+				e.printStackTrace();
+			}
+	}
+
+	@Override
+	public String help() throws RemoteException {
+		// Return help options
+		return null;
+	}
+
+}
