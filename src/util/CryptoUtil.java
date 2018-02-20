735416768aaf7845d3246a92049029485fb19691
dikelito <dikelito@tutamail.com>
2018-02-20 23:15:29 +0200

    Changed CryptoUtil package to util

---
+++ src/util/CryptoUtil.java
@@ -0,0 +1,20 @@
+package util;
+
+import java.security.MessageDigest;
+import javax.xml.bind.DatatypeConverter;
+
+public class CryptoUtil {
+
+	static String hash(String data) {
+
+		MessageDigest md;
+		byte[] hash = null;
+		try {
+			md = MessageDigest.getInstance("SHA-256");
+			hash = md.digest(data.getBytes("UTF-8"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return DatatypeConverter.printHexBinary(hash);
+	}
+}
