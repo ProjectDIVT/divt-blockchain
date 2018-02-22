5f4a7c2f88e1697dcd789cc3543b9700c1e03771
Ivan Spasov <ivanss9@abv.bg>
2018-02-22 16:07:17 +0200

    Block class
    no idea дали всъщност е каквото ни трябва?

---
+++ Block.java
@@ -0,0 +1,15 @@
+
+public class Block {
+	String hash;
+	int confirmations;
+	int height;
+	int size;
+	int time;
+	
+	String prevBlockHash;
+	float supply;
+	float difficulty;
+	
+	String transaction;
+	
+}
